7331e762e37df4f862e36b9da9d6e58744966c01
SSanchez7 <sa.sanchez@outlook.com>
2021-04-30 17:36:52 -0400

    update comment-modal

--- Front-end/formulario.js
+++ Front-end/formulario.js
@@ -1,33 +1,11 @@
-function cerrarComentario(){
-    let modal = document.getElementsByClassName("modal");
-    modal.style.display = "none";
-}
 
-function abrirComentario(){
-    let modal = document.getElementsByClassName("modal");
-    modal.style.display = "block";
-}
-
-
-var modal = document.getElementsById("m1");
-
-var cerrar = document.getElementsById("close");
-
-var enviar = document.getElementsById("enviar_comentario");
-
-var abrir = document.getElementsById("abrir_modal");
-
-
-abrir.onclick = function() {
-    modal.style.display = "block";
-}
-
-function confbox(){
-    let cont = document.getElementById("abrir_modal");
+function open_comment_modal(modal){
+    let cont = document.getElementById(modal);
     cont.style.display = "block";
-
+}
+function close_comment_modal(modal){
+	let cont = document.getElementById(modal)
+	cont.style.display = "none";
 }
 
-cerrar.onclick = function() {
-    modal.style.display = "none";
-}
+
